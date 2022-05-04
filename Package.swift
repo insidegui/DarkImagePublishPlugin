52fcd7f401c7498e0bb59c44da5e40838be2b3b6
Nikolai Trukhin <coolone.official@gmail.com>
2022-05-04 21:53:03 +0300

    Update Swift/platform versions for Publish 0.9.0
    Since Publish now requires Swift 5.5 and macOS 12, update this
    plugin accordingly.

--- Package.swift
+++ Package.swift
@@ -1,17 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DarkImagePublishPlugin",
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "DarkImagePublishPlugin",
             targets: ["DarkImagePublishPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.9.0"),
     ],
     targets: [
         .target(
